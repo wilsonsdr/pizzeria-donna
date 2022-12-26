b480e7aa56b8af7fd509376740c6b34004544337
Wilson <wilsonsdr@gmail.com>
2022-12-26 17:51:49 -0300

    chore: atualizacao do script

--- js/watsonAssistant.js
+++ js/watsonAssistant.js
@@ -1,11 +1,11 @@
 window.watsonAssistantChatOptions = {
-  integrationID: "6182c7df-2bca-41ce-9a1b-a9eaa2edd956", // The ID of this integration.
+  integrationID: "e8cc4174-7c74-4592-b2d0-0e9ad1f0b948", // The ID of this integration.
   region: "us-south", // The region your integration is hosted in.
-  serviceInstanceID: "a3380792-edb3-4e49-af8d-cf61531466d4", // The ID of your service instance.
-  onLoad: function(instance) { instance.render(); }
+  serviceInstanceID: "c0836a95-5090-48b4-81e9-3883edf774ea", // The ID of your service instance.
+  onLoad: function (instance) { instance.render(); }
 };
-setTimeout(function(){
-  const t=document.createElement('script');
-  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+setTimeout(function () {
+  const t = document.createElement('script');
+  t.src = "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
   document.head.appendChild(t);
 });
